7c6ea17d8066cdc752eba61d8794a6affb554de7
KikolasDEV <kikoamarillo94@gmail.com>
2024-09-17 13:06:36 +0200

    Startup Project

---
+++ main.py
@@ -0,0 +1,4 @@
+'''
+You will use what you've learnt to create a text-based (command line) program that takes any String input and converts it into Morse Code.
+https://en.wikipedia.org/wiki/Morse_code
+'''

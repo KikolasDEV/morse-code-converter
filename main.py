77aca07794f353b0be0540bc16f79fb1fdff8be1
KikolasDEV <kikoamarillo94@gmail.com>
2024-11-08 13:56:47 +0100

    Adding its own translation below the Morse Code

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-'''
-You will use what you've learnt to create a text-based (command line) program that takes any String input and converts it into Morse Code.
-https://en.wikipedia.org/wiki/Morse_code
-'''
+morse_code = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'Ñ': '--.--', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--',
+    'X': '-..-', 'Y': '-.-', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', '!': '-.-.--'
+}
 
-morse_code = {'A': '.-', 'B': '-...', 'C': '–.–.', 'D': '–..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
-              'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'Ñ': '--.--', 'O': '---',
-              'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--',
-              'X': '-..-', 'Y': '-.-', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
-              '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
-              '.': '.-.-.-', ',': '--..--', '?': '..--..', '!': '-.-.--'}
 
 def convert_sentence(input_text):
-
     morse_sentence = ""
+    letters_line = ""
     for letter in input_text:
-        if letter.upper() in morse_code:
-            morse_sentence += morse_code[letter.upper()]
-        elif letter == " ":
-            morse_sentence += " "
+        if letter == " ":
+            morse_sentence += "(space) "
+            letters_line += "        "
+        else:
+            morse_code_char = morse_code.get(letter.upper(), "")
+            morse_sentence += morse_code_char + " "
+            letters_line += letter.upper() + " " * (len(morse_code_char) - 1) + " "
 
-    return morse_sentence
+    dashes_line = ''.join(['-' if c != ' ' else ' ' for c in morse_sentence])
+
+    return morse_sentence.strip(), dashes_line.strip(), letters_line.strip()
 
 
 text_input = input("Introduzca el texto que desea convertir a Codigo Morse: ")
-final_result = convert_sentence(text_input)
-print(f"El texto traducido a Codigo Morse es el siguiente: {final_result}")
+morse_result, dashes_result, letters_result = convert_sentence(text_input)
+
+print(" ######################################## MORSE CODE CONVERSION ##############################################")
+print(morse_result)
+print(dashes_result)
+print(letters_result)
